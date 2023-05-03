6c3e9ae58e07b1e8371ab1ec89030aa49ceed88f
Tiqur <trevorbrage02@gmail.com>
2023-05-03 02:52:53 -0700

    Added hit object fade in animation

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,55 @@
 import * as THREE from 'three';
 
 const timeStart = Date.now(); 
+const ar = 9;
+
+export default function lerp(oldValue: number, oldMin: number, oldMax: number, newMin: number, newMax: number) {
+  const oldRange = (oldMax - oldMin)  
+  const newRange = (newMax - newMin)  
+  const newValue = (((oldValue - oldMin) * newRange) / oldRange) + newMin
+  return newValue; 
+}
+
+function calculatePreemptTime(ar: number) {
+  if (ar < 5) {
+    return 1200 + 600 * (5 - ar) / 5;
+  } else if (ar > 5) {
+    return 1200 - 750 * (ar - 5) / 5;
+  } else {
+    return 1200;
+  }
+}
+
+function calculateFadeInTime(ar: number) {
+  if (ar < 5) {
+    return 800 + 400 * (5 - ar) / 5;
+  } else if (ar > 5) {
+    return 800 - 500 * (ar - 5) / 5;
+  } else {
+    return 800;
+  }
+}
 
 class HitObject {
   geometry: THREE.CircleGeometry;
   material: THREE.MeshBasicMaterial;
   object: THREE.Mesh;
+  preempt_start: number;
+  fade_in_end: number;
   time: number;
 
   constructor(time: number, coords: Coords) {
     this.geometry = new THREE.CircleGeometry(1, 64);
-    this.material = new THREE.MeshBasicMaterial({color: "#433F81", opacity: 0.8, transparent: true});
+    this.material = new THREE.MeshBasicMaterial({color: "#433F81", opacity: 0, transparent: true});
     this.object = new THREE.Mesh(this.geometry, this.material);
     this.object.position.set(coords.x, coords.y, 0);
     this.time = time;
+    this.preempt_start = calculatePreemptTime(ar);
+    this.fade_in_end = calculateFadeInTime(ar);
   }
 
-  updateTime(time: number) {
-
+  setOpacity(value: number) {
+    this.material.opacity = value;
   }
 
   getObject() {
@@ -47,24 +79,35 @@ document.body.appendChild(renderer.domElement);
 
 const objects: HitObject[] = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 100; i++) {
   // Create objects
   const object = new HitObject((i*200)+1000, {x: genRandNum(-2, 2), y: genRandNum(-2, 2)});
   objects.push(object);
+  scene.add(object.getObject());
 }
 
 
 const render = () => {
   requestAnimationFrame(render);
-  const timeElapsed = Date.now() -timeStart;
+  const timeElapsed = Date.now() - timeStart;
 
   for (const obj of objects) {
-    if (timeElapsed > obj.time - 1000) {
-      scene.add(obj.getObject());
+    // Show obj if within time bounds
+    if (timeElapsed >= obj.time - obj.preempt_start) {
+      let newOpacity: number = 0;
+
+      if (timeElapsed < obj.time && timeElapsed > obj.preempt_start)
+        newOpacity = Math.max(Math.min(lerp(timeElapsed, obj.time - obj.preempt_start, obj.time - obj.fade_in_end, 0, 0.8), 0.8), 0);
+      else if (timeElapsed >= obj.fade_in_end)
+        newOpacity = 0.8;
+
+      console.log(newOpacity);
+
+      obj.setOpacity(newOpacity);
     }
 
-    // Remove and delete if time > object's local time
-    if (timeElapsed > obj.time + 1000) {
+    // Delete obj
+    if (timeElapsed > obj.time) {
       scene.remove(obj.getObject());
       obj.delete();
     }
